e7ef8d4064a3eaa9224b8fdc7ded61cc96abc768
Arnaud Wurmel <arnaud.wurmel1@gmail.com>
2026-05-05 11:33:16 +0200

    Initial Commit

---
+++ CoordinatorDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorDemoApp
+//
+//  Created by Arnaud Wurmel on 05/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
